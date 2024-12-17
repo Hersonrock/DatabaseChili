accac1834d828790f70c4009c4763eb7945ad9fd
Hersonrock <hjpg20@gmail.com>
2024-12-17 11:34:36 -0500

    Fixes issue with & on conditional and a missing }

--- DatabaseChili.cpp
+++ DatabaseChili.cpp
@@ -5,22 +5,22 @@ int main()
 {
         bool isRunning = true;
         char opt = '\0';
-        while(isRunning){
-                if(opt == 's'){
-                        return 0;
-                }
-                std::cout << "(l)load (s)ave (a)dd (q)uit (p)rint" 
-                          << std::endl; 
-                opt = _getch();
+        while (isRunning) {
+            if (opt == 's') {
+                return 0;
+            }
+            std::cout << "(l)load (s)ave (a)dd (q)uit (p)rint"
+                << std::endl;
+            opt = _getch();
 
-                if( (opt != 'l') &
-                    (opt != 's') &
-                    (opt != 'a') &
-                    (opt != 'q') &
-                    (opt != 'p')){
-                        std::cout << "Invalid input.."<< std::endl; 
-                        continue;
-                }
-
+            if ((opt != 'l') &&
+                (opt != 's') &&
+                (opt != 'a') &&
+                (opt != 'q') &&
+                (opt != 'p')) {
+                std::cout << "Invalid input.." << std::endl;
+                continue;
+            }
+        }
         return 0;
 }
