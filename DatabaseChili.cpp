42975c61481f27d48f590b181f58c1816b658534
Rynok26 <hjpg200@gmail.com>
2024-12-17 11:29:48 -0500

    Adds loop lifetime logic and basic input checking

--- DatabaseChili.cpp
+++ DatabaseChili.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <conio.h>
 
 int main()
 {
         bool isRunning = true;
+        char opt = '\0';
         while(isRunning){
+                if(opt == 's'){
+                        return 0;
+                }
                 std::cout << "(l)load (s)ave (a)dd (q)uit (p)rint" 
                           << std::endl; 
-        }
+                opt = _getch();
+
+                if( (opt != 'l') &
+                    (opt != 's') &
+                    (opt != 'a') &
+                    (opt != 'q') &
+                    (opt != 'p')){
+                        std::cout << "Invalid input.."<< std::endl; 
+                        continue;
+                }
 
         return 0;
 }
