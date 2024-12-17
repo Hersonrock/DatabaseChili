8bb175b3a0c1b52a4080ef1f0b4b92c0ad88e99f
Rynok26 <hjpg200@gmail.com>
2024-12-17 11:48:16 -0500

    Refactoring if conditional for a switch

--- DatabaseChili.cpp
+++ DatabaseChili.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include <conio.h>
 
-int main()
-{
+int main(){
         bool isRunning = true;
         char opt = '\0';
         while (isRunning) {
-            if (opt == 'q') {
-                return 0;
-            }
             std::cout << "(l)load (s)ave (a)dd (q)uit (p)rint"
                 << std::endl;
             opt = _getch();
-
-            if ((opt != 'l') &&
-                (opt != 's') &&
-                (opt != 'a') &&
-                (opt != 'q') &&
-                (opt != 'p')) {
-                std::cout << "Invalid input.." << std::endl;
-                continue;
+            switch(opt){
+                    case l:
+                            std::cout << "Loading..." << std::endl;
+                            break;        
+                    case s:
+                            std::cout << "Saving..." << std::endl;
+                            break;        
+                    case a:
+                            std::cout << "Adding..." << std::endl;
+                            break;        
+                    case q:
+                            std::cout << "Bye..." << std::endl;
+                            isRunning = false;
+                            break;        
+                    case p:
+                            std::cout << "Printing..." << std::endl;
+                            break;        
             }
         }
         return 0;
